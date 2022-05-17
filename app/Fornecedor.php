70ab0fd50c716c2fc8ad5de085c5f50ead668578
Rafael Hitoshi <npchitoshi@gmail.com>
2022-05-16 22:18:59 -0300

    Laravel :: Inicializando projeto no git

---
+++ app/Fornecedor.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+//fornecedors
+//fornecedores
+
+class Fornecedor extends Model
+{
+    use SoftDeletes;
+    protected $table = 'fornecedores';
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
+}
--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -7,6 +7,23 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
     public function contato() {
-        return view('site.contato');
+
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação'
+        ];
+
+        return view('site.contato', ['titulo' => 'Contato (teste)', 'motivo_contatos' => $motivo_contatos]);
+    }
+
+    public function salvar(Request $request) {
+        $request->validate([
+            'nome' => 'required|min:3|max:40',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required|max:200'
+        ]);
     }
 }
